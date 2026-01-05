5fd059d09fe5769e7f9a58dc949b7c37f9feb67f
Felix Zeilinger <zeilinger.felix@gmx.at>
2026-01-06 00:09:09 +0100

    Update control_task.cpp

--- Code/src/tasks/control_task.cpp
+++ Code/src/tasks/control_task.cpp
@@ -2,12 +2,34 @@
 #include "tasks.h"
 
 #define PLACEHOLDER (0) //TODO: implement actual reading 
-#define DELTA_T (1)// TODO: adjust according to task frequency
-#define ProportionalVerstaerkung_Kp (1.0f)
-#define IntegralVerstaerkung_Ki (1.0f)
-#define DerivativeVerstaerkung_Kd (1.0f)
 
-#define D_FILETR_ALPHA 0.7//TODO: tune bzw. ideet calculate
+struct PID
+{
+       //static because multiple calls
+        static float Fuehrungsgroesse_w ;
+        static float Regelgroesse_x;
+        static float Regeldiff_e ;
+        static float last_x;
+        static float Integral_Ie;
+        static float rawDerivative_De;
+        static float filtertDerivative_fDe;
+        static float lastfiltertDerivative_lfDe;
+        static float p;
+        static float i;
+        static float d;
+        static float Regelausgangsgr_m;
+        static bool saturated = false; 
+        static float DELTA_T=1;                     // TODO: adjust according to task frequency
+        static float ProportionalVerstaerkung_Kp;   //tune
+        static float IntegralVerstaerkung_Ki    ;
+        static float DerivativeVerstaerkung_Kd  ;
+        static float D_FILETR_ALPHA= 0.7;           //TODO: tune bzw. ideet calculate
+
+
+
+    /* data */
+};
+
 
 
 
@@ -17,25 +39,40 @@ void TaskControl(void *pvParameters)
 {
     TickType_t last = xTaskGetTickCount();
 
-       //TODO: Solve trogh structs
 
-        //static because multiple calls
-        static float Fuehrungsgroesse_w =0.0f;
-        static float Regelgroesse_x=0.0f;
-        static float Regeldiff_e =0.0f;
-        static float last_x=0.0f;
-        static float Integral_Ie=0.0f;
-        static float rawDerivative_De=0.0f;
-        static float filtertDerivative_De=0.0f;
-        static float lastfiltertDerivative_De=0.0f;
-        static float p;
-        static float i;
-        static float d;
-        static float Regelausgangsgr_m;
+        int64_t now_us = esp_timer_get_time();
+        dt = (now_us - last_us) * 1e-6f;
+        last_us = now_us;
 
+    //   DEMO PID 
+         
+        PID demoPID;
+
+        demoPID.Fuehrungsgroesse_w =0.0f;
+        demoPID.Regelgroesse_x=0.0f;
+        demoPID.Regeldiff_e =0.0f;
+        demoPID.last_x=0.0f;
+        demoPID.Integral_Ie=0.0f;
+        demoPID.rawDerivative_De=0.0f;
+        demoPID.filtertDerivative_fDe=0.0f;
+        demoPID.lastfiltertDerivative_lfDe=0.0f;
+        demoPID.p;
+        demoPID.i;
+        demoPID.d;
+        demoPID.Regelausgangsgr_m;
+        demoPID.saturated = false;  
+
+        //Konstanten
+        demoPID. DELTA_T=0.002f;                     // TODO: adjust according to task frequency
+        demoPID. ProportionalVerstaerkung_Kp=0.0f;   
+        demoPID. IntegralVerstaerkung_Ki    =0.0f;
+        demoPID. DerivativeVerstaerkung_Kd  =0.0f;
+        demoPID.D_FILETR_ALPHA = 0.7;         
     for (;;)
     {
-        ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(2));
+        ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(2)); //TODO: anscheinded mit vTaskDelayUntil zusammen doof
+
+
         //TODO: for each loop think if I and D are needed
 
 
@@ -43,41 +80,42 @@ void TaskControl(void *pvParameters)
         // run PID  o.AE.
         // TODO; Concept - TOGETHER
         //TODO: decide if fastforwarded 
-        //TODO: cascade also rate control?
+        //TODO: cascade also rate control? - split into different tasks
 
 
 
+        demoPID.Fuehrungsgroesse_w = PLACEHOLDER;//TODO: implement actual reading and writing
+        demoPID.Regelgroesse_x = PLACEHOLDER;
+        demoPID.Regeldiff_e = demoPID.Fuehrungsgroesse_w - demoPID.Regelgroesse_x;
+        demoPID.saturated = false; //TODO: implement saturation detection
 
-        Fuehrungsgroesse_w = PLACEHOLDER;
-        Regelgroesse_x = PLACEHOLDER;
-        Regeldiff_e = Fuehrungsgroesse_w - Regelgroesse_x;
-        Integral_Ie += Regeldiff_e * (DELTA_T);
-        // TODO:Anti-windup
-       
+        
+        //TODO: better anti windup aproche
+        if(!saturated) //TODO: check saturation condition  -:https://youtu.be/NVLXCwc8HzM?si=FpmdmcUAy8FntkJC&t=386
+        {
+        demoPID.Integral_Ie += demoPID.Regeldiff_e * (DELTA_T);
+        }
 
-
-
-
-        rawDerivative_De = (Regelgroesse_x - last_x) / (DELTA_T);
-        last_x     = Regelgroesse_x;  
-        filtertDerivative_De = D_FILETR_ALPHA * rawDerivative_De + (1 - D_FILETR_ALPHA) * lastfiltertDerivative_De;
+        demoPID.rawDerivative_De = (demoPID.Regelgroesse_x - demoPID.last_x) / (DELTA_T);
+        demoPID.last_x     = demoPID.Regelgroesse_x;  
+        demoPID.filtertDerivative_fDe = D_FILETR_ALPHA * demoPID.rawDerivative_De + (1 - D_FILETR_ALPHA) * demoPID.lastfiltertDerivative_lfDe;
         //https://www.youtube.com/watch?v=HJ-C4Incgpw - die haben den ganzen fourje spaß imlementiert um unsr Alpha zu finden
-        lastfiltertDerivative_De = filtertDerivative_De;
+        demoPID.lastfiltertDerivative_lfDe = demoPID.filtertDerivative_fDe;
 
 
 
+        demoPID.p = demoPID.Regeldiff_e * ProportionalVerstaerkung_Kp;
+        demoPID.i = demoPID.Integral_Ie * IntegralVerstaerkung_Ki;
+        demoPID.d = demoPID.filtertDerivative_fDe * DerivativeVerstaerkung_Kd;
+        demoPID.Regelausgangsgr_m = demoPID.p+demoPID.i+demoPID.d;//might fastforwarded 
 
-        p = Regeldiff_e * ProportionalVerstaerkung_Kp;
-        i = Integral_Ie * IntegralVerstaerkung_Ki;
-        d = filtertDerivative_De * DerivativeVerstaerkung_Kd;
-        Regelausgangsgr_m = p+i+d;//might fastforwarded 
+        
+        //gpt concept of anti windup
+        out = clamp(out, MIN_CMD, MAX_CMD);
+        saturated = (out != unclamped_out);
 
-  
-
-
-
-       
-        // mix motors
+        
+        // mix motors - probaly just default quadcopter x config
 
         // output PWM
         vTaskDelayUntil(&last, pdMS_TO_TICKS(2)); // ~500Hz // TODO: adjust for all tasks
