6d61df6b1361534295a73c5f02643c1f5d758b07
Felix Zeilinger <zeilinger.felix@gmx.at>
2026-01-21 22:22:02 +0100

    control design update

--- Code/src/tasks/control_task.cpp
+++ Code/src/tasks/control_task.cpp
@@ -5,56 +5,51 @@
 float scaleOutput(float voltage);
 
 
-
-class P{
+class P {
     private:
-        float Regeldiff_e ;
+        float Regeldiff_e;
         float p; 
         float ProportionalVerstaerkung_Kp;   
-        float Fuehrungsgroesse_w ;
+        float Fuehrungsgroesse_w;
         float Regelgroesse_x;
+
+        float calculateP(float error) {
+            return error * ProportionalVerstaerkung_Kp;
+        }
 
     public:
         float Regelausgangsgr_m;
 
-        P(float kp){
-        Fuehrungsgroesse_w =0.0f;
-        Regelgroesse_x=0.0f;
-        Regeldiff_e =0.0f;
-        p=0.0f;
-        Regelausgangsgr_m=0.0f;
+        P(float kp)
+            : Regeldiff_e(0.0f), p(0.0f), ProportionalVerstaerkung_Kp(kp),
+              Fuehrungsgroesse_w(0.0f), Regelgroesse_x(0.0f), Regelausgangsgr_m(0.0f) {}
 
-        //Konstanten
-        ProportionalVerstaerkung_Kp=kp;   
-    }
-
-     
-  
-        void pStep(float dt){
+        void step(float dt) {
             Regeldiff_e = Fuehrungsgroesse_w - Regelgroesse_x;
-            p = Regeldiff_e * ProportionalVerstaerkung_Kp;
+            p = calculateP(Regeldiff_e);
             Regelausgangsgr_m = p;       
         }
-        void reseter() {
+
+        void reset() {
+            p = 0.0f;
         }
+
         void setInput(float w, float x) {
             Fuehrungsgroesse_w = w;
             Regelgroesse_x = x;
         }   
+
         void getOutput(float &out) {
             out = Regelausgangsgr_m;
         }
 };
 
 
-class PID{  
-    //All we need i guess: https://www.linkedin.com/pulse/mastering-pid-control-drones-from-intuition-dr-ma-mohin-zbjqe/ 
- 
-    
+class PID {
     private:
-        float Fuehrungsgroesse_w ;
+        float Fuehrungsgroesse_w;
         float Regelgroesse_x;
-        float Regeldiff_e ;
+        float Regeldiff_e;
         float last_x;
         float Integral_Ie;
         float rawDerivative_De;
@@ -63,98 +58,81 @@ class PID{
         float p;
         float i;
         float d; 
-        bool  saturated ;
+        bool saturated;
         float unclamped_m;
         float ProportionalVerstaerkung_Kp;  
-        float IntegralVerstaerkung_Ki    ;
-        float DerivativeVerstaerkung_Kd  ;
+        float IntegralVerstaerkung_Ki;
+        float DerivativeVerstaerkung_Kd;
         float D_FILETR_ALPHA;          
-        float OUT_MAX ; 
-        float OUT_MIN ; 
+        float OUT_MAX; 
+        float OUT_MIN;
 
-        int64_t last_us = 0; 
-
-    public:
-            float Regelausgangsgr_m; 
-
-       
-
-    PID(float kp, float ki, float kd, float alpha,float out_min=0.0f, float out_max=1.0f){
-        Fuehrungsgroesse_w =0.0f;
-        Regelgroesse_x=0.0f;
-        Regeldiff_e =0.0f;
-        last_x=0.0f;
-        Integral_Ie=0.0f;
-        rawDerivative_De=0.0f;
-        filtertDerivative_fDe=0.0f;
-        lastfiltertDerivative_lfDe=0.0f;
-        p=0.0f;
-        i=0.0f;
-        d=0.0f;
-        Regelausgangsgr_m=0.0f;
-        unclamped_m=0.0f;
-        saturated = false;  
-
-        //Konstanten
-        ProportionalVerstaerkung_Kp=kp;   
-        IntegralVerstaerkung_Ki    =ki;
-        DerivativeVerstaerkung_Kd  =kd;
-        D_FILETR_ALPHA =alpha;    
-        OUT_MAX = out_max;
-        OUT_MIN = out_min; 
-    }
-    void setInput(float w, float x) {
-    Fuehrungsgroesse_w = w;
-    Regelgroesse_x = x;
-    }
-    void getOutput(float &out) {
-    out = Regelausgangsgr_m;
-    }
-
-     
-  
-    void pidStep(DELTA_T){
-
-        Regeldiff_e = Fuehrungsgroesse_w - Regelgroesse_x;
-        
-        if(!saturated) 
-        {
-        Integral_Ie += Regeldiff_e * (DELTA_T);
+        float calculateP(float error) {
+            return error * ProportionalVerstaerkung_Kp;
         }
 
-        rawDerivative_De = (Regelgroesse_x - last_x) / (DELTA_T);
-        last_x     = Regelgroesse_x;
-        filtertDerivative_fDe = D_FILETR_ALPHA * rawDerivative_De + (1 - D_FILETR_ALPHA) * lastfiltertDerivative_lfDe;
-        //https://www.youtube.com/watch?v=HJ-C4Incgpw - die haben den ganzen fourje spaß imlementiert um unsr Alpha zu finden
-        lastfiltertDerivative_lfDe = filtertDerivative_fDe;
+        float calculateI(float error, float dt) {
+            if (!saturated) {
+                Integral_Ie += error * dt;
+            }
+            return Integral_Ie * IntegralVerstaerkung_Ki;
+        }
 
-        p = Regeldiff_e * ProportionalVerstaerkung_Kp;
-        i = Integral_Ie * IntegralVerstaerkung_Ki;
-        d = filtertDerivative_fDe * DerivativeVerstaerkung_Kd;
-        Regelausgangsgr_m = p+i-d;//might fastforwarded 
+        float calculateD(float current_x, float dt) {
+            rawDerivative_De = (current_x - last_x) / dt;
+            filtertDerivative_fDe = D_FILETR_ALPHA * rawDerivative_De + 
+                                    (1.0f - D_FILETR_ALPHA) * lastfiltertDerivative_lfDe;
+            lastfiltertDerivative_lfDe = filtertDerivative_fDe;
+            return filtertDerivative_fDe * DerivativeVerstaerkung_Kd;
+        }
 
+        void clampOutput() {
+            unclamped_m = Regelausgangsgr_m;
+            if (Regelausgangsgr_m > OUT_MAX) Regelausgangsgr_m = OUT_MAX;
+            if (Regelausgangsgr_m < OUT_MIN) Regelausgangsgr_m = OUT_MIN;
+            saturated = (Regelausgangsgr_m != unclamped_m);
+        }
 
-                
-        
-        //TODO:THINK better anti windup aproche needed?
-        //gpt concept of anti windup
+    public:
+        float Regelausgangsgr_m;
 
-        unclamped_m = Regelausgangsgr_m;
-        if (Regelausgangsgr_m > OUT_MAX) Regelausgangsgr_m = OUT_MAX;
-        if (Regelausgangsgr_m < OUT_MIN) Regelausgangsgr_m = OUT_MIN;
+        PID(float kp, float ki, float kd, float alpha, float out_min = 0.0f, float out_max = 1.0f)
+            : Fuehrungsgroesse_w(0.0f), Regelgroesse_x(0.0f), Regeldiff_e(0.0f),
+              last_x(0.0f), Integral_Ie(0.0f), rawDerivative_De(0.0f),
+              filtertDerivative_fDe(0.0f), lastfiltertDerivative_lfDe(0.0f),
+              p(0.0f), i(0.0f), d(0.0f), saturated(false), unclamped_m(0.0f),
+              ProportionalVerstaerkung_Kp(kp), IntegralVerstaerkung_Ki(ki),
+              DerivativeVerstaerkung_Kd(kd), D_FILETR_ALPHA(alpha),
+              OUT_MAX(out_max), OUT_MIN(out_min), Regelausgangsgr_m(0.0f) {}
 
+        void step(float dt) {
+            Regeldiff_e = Fuehrungsgroesse_w - Regelgroesse_x;
+            
+            p = calculateP(Regeldiff_e);
+            i = calculateI(Regeldiff_e, dt);
+            d = calculateD(Regelgroesse_x, dt);
+            
+            Regelausgangsgr_m = p + i - d;
+            clampOutput();
+            
+            last_x = Regelgroesse_x;
+        }
 
-        saturated = (Regelausgangsgr_m != unclamped_m);
-        
-    
-    }    
+        void reset() {
+            Integral_Ie = 0.0f;
+            last_x = Regelgroesse_x;
+            lastfiltertDerivative_lfDe = 0.0f;
+            saturated = false;
+        }
 
-    void reseter() {
-    Integral_Ie = 0.0f;
-    last_x = Regelgroesse_x;
-    lastfiltertDerivative_lfDe = 0.0f;
-    }   
+        void setInput(float w, float x) {
+            Fuehrungsgroesse_w = w;
+            Regelgroesse_x = x;
+        }
 
+        void getOutput(float &out) {
+            out = Regelausgangsgr_m;
+        }
 };
 
 
@@ -205,7 +183,7 @@ void TaskControl(void *pvParameters)
 
 
         ZspeedPID.setInput(PLACEHOLDER, PLACEHOLDER);
-        ZspeedPID.pidStep(DELTA_T);
+        ZspeedPID.step(DELTA_T);
         float throttle = ZspeedPID.Regelausgangsgr_m; 
 
 
@@ -215,40 +193,29 @@ void TaskControl(void *pvParameters)
         float currentYaw   = PLACEHOLDER; 
 
         // Update PID controllers for attitude
-       
-
-        // Update for Führungsgroesse_w from desired attitude commands
-       
-        pitchPID.setInput(currentPitch, PLACEHOLDER);
-        rollPID.setInput(currentRoll, PLACEHOLDER);
-        yawPID.setInput(currentYaw, PLACEHOLDER);
-
-
-
+        pitchPID.setInput(PLACEHOLDER, currentPitch);
+        rollPID.setInput(PLACEHOLDER, currentRoll);
+        yawPID.setInput(PLACEHOLDER, currentYaw);
 
         // TODO: Call less often
-        pitchPID.pStep(DELTA_T);
-        rollPID.pStep(DELTA_T);
-        yawPID.pStep(DELTA_T);
-
-
+        pitchPID.step(DELTA_T);
+        rollPID.step(DELTA_T);
+        yawPID.step(DELTA_T);
 
         // Read current rates from gyroscope
-        float currentRatePitch = PLACEHOLDER; //TODO: implement actual reading - if given use current rates from gyroscope
+        float currentRatePitch = PLACEHOLDER; //TODO: implement actual reading
         float currentRateRoll  = PLACEHOLDER;
         float currentRateYaw   = PLACEHOLDER;
-
 
         // Use attitude PID outputs as setpoints for rate controllers
         ratePitchPID.setInput(pitchPID.Regelausgangsgr_m, currentRatePitch);
         rateRollPID.setInput(rollPID.Regelausgangsgr_m, currentRateRoll);
         rateYawPID.setInput(yawPID.Regelausgangsgr_m, currentRateYaw);
 
-      
-        // Update PID controllers for rates via setInput and pidStep
-        ratePitchPID.pidStep(DELTA_T);
-        rateRollPID.pidStep(DELTA_T);
-        rateYawPID.pidStep(DELTA_T);
+        // Update PID controllers for rates
+        ratePitchPID.step(DELTA_T);
+        rateRollPID.step(DELTA_T);
+        rateYawPID.step(DELTA_T);
         // Compute motor commands based on rate PID outputs
         float motorCommandPitch = ratePitchPID.Regelausgangsgr_m;
         float motorCommandRoll  = rateRollPID.Regelausgangsgr_m;
@@ -268,11 +235,11 @@ void TaskControl(void *pvParameters)
         float scalingFactor =  scaleOutput(voltage);
 
         float motorOutputs[4];
-            for (int i = 0; i < 4; i++) {
-                motorOutputs[i] = motorOutputsRaw[i] * scalingFactor;        
-                if (motorOutputs[i] > 1.0f) motorOutputs[i] = 1.0f;
-                if (motorOutputs[i] < 0.0f) motorOutputs[i] = 0.0f;
-            }
+        for (int i = 0; i < 4; i++) {
+            motorOutputs[i] = motorOutputsRaw[i] * scalingFactor;        
+            if (motorOutputs[i] > 1.0f) motorOutputs[i] = 1.0f;
+            if (motorOutputs[i] < 0.0f) motorOutputs[i] = 0.0f;
+        }
         
         // TODO: clamp motors and scale 
         // TODO: output PWM
