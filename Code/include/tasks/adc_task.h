94419b3699f9aefc57aa17861bd8792ec1fd8fb1
Anothyre <anothyre@gmail.com>
2026-02-11 10:17:55 +0100

    Implement reading current and battery voltage with ADC

--- Code/include/tasks/adc_task.h
+++ Code/include/tasks/adc_task.h
@@ -6,4 +6,18 @@
 
 void TaskADC(void *pvParameters);
 
+#define ADC_VOLTAGE_PIN         5
+#define ADC_CURRENT_PIN         6
+#define ADC_GAIN                ADC_11db
+#define ADC_RESOLUTION          12
+#define ADC_MAX_VALUE           4095.0f     // 2^12
+#define ADC_REF_VOLTAGE         3.3f
+
+#define VOLTAGE_DIVIDER_RATIO   5.3         // (34kOhm + 10kOhm) / 10kOhm
+
+#define SHUNT_AMP_RATIO         50          // 50V/V
+#define SHUNT_RESISTANCE        0.5e-3      // 0.5mOhm
+#define INITIAL_CURRENT         0           // 0A
+#define INITIAL_BATTERY_VOLTAGE         16.8f       // 4S LIPO full voltage
+
 #endif // ADC_TASK_H
--- Code/src/tasks/adc_task.cpp
+++ Code/src/tasks/adc_task.cpp
@@ -1,12 +1,39 @@
+#include "Arduino.h"
 #include "tasks/adc_task.h"
-// analog digital converter task for ???
 
 void TaskADC(void *pvParameters)
 {
+
+    // Init ADC
+    analogReadResolution(ADC_RESOLUTION);                   // 0–4095
+    analogSetPinAttenuation(ADC_CURRENT_PIN, ADC_GAIN);     // up to ~3.3V
+    analogSetPinAttenuation(ADC_VOLTAGE_PIN, ADC_GAIN);
+
     for (;;)
     {
-        //BRANDNER
-        // TODO: ADC reading for current/voltage monitoring
+        // Init previous data (Assuming full charge and stil Motors)
+        static float p_current = INITIAL_CURRENT;
+        static float p_batteryVoltage = INITIAL_BATTERY_VOLTAGE;
+        
+        // Read raw Data
+        uint16_t rawCurrent     = analogRead(ADC_CURRENT_PIN);
+        uint16_t rawVoltage     = analogRead(ADC_VOLTAGE_PIN);
+
+        // Calculate actual current
+        float currentVoltage    = (rawCurrent / ADC_MAX_VALUE) * ADC_REF_VOLTAGE;
+        float shuntVoltage      = currentVoltage / SHUNT_AMP_RATIO;
+        float current           = shuntVoltage / SHUNT_RESISTANCE;
+
+        // Calculate actual voltage
+        float busVoltage        = (rawVoltage / ADC_MAX_VALUE) * ADC_REF_VOLTAGE;
+        float batteryVoltage    = busVoltage * VOLTAGE_DIVIDER_RATIO;
+
+        // Average with last value for smoothing
+        current                 = (current + p_current) / 2.0f;
+        p_current               = current;
+        batteryVoltage          = (batteryVoltage + p_batteryVoltage) / 2.0f;
+        p_batteryVoltage        = batteryVoltage;
+
         vTaskDelay(pdMS_TO_TICKS(10)); // 100Hz
     }
 }
